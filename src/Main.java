3d0fbb5578bac348268f55d939ec11617b84c5df
JoannaRatajczak <joanna.jr.ratajczak@gmail.com>
2019-05-19 14:17:43 +0200

    warunek 2 i 3 jest taki sam. a==b -> a^2 czyli równie dobrze można zostawić a*b i zaoszczędzić linijek

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        Method method = new Method();
+        int x = 8;
+        int y = 5;
+        System.out.println(method.method(x, y));
+        x = 5;
+        y = 5;
+        System.out.println(method.method(x, y));
+        x = 1;
+        y = 10;
+        System.out.println(method.method(x, y));
+
+    }
+}
---
+++ src/Method.java
@@ -0,0 +1,14 @@
+public class Method {
+
+    int method(int a, int b) {
+
+        if (a > b) {
+            return a + b;
+        } else return a * b;
+    }
+
+    public Method() {
+    }
+}
+
+
